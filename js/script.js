07d9a71e598ac51e11e0d09a18f923bad91c1055
Faitsumaru <kiryakov.g.m@gmail.com>
2025-01-28 19:07:18 +0300

    change bg & title logo script done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ function toggleVideo() {
     video.pause()
     trailer.classList.toggle('active')
 }
+
+
+function changeBg(bg, title) {
+    const banner = document.querySelector('.main__banner')
+    const contents = document.querySelectorAll('.main__content')
+
+    banner.style.background = `url("../images/movies/${bg}")`
+    banner.style.backgroundSize = 'cover'
+    banner.style.backgroundPosition = 'center'
+
+    contents.forEach(cnt => {
+        cnt.classList.remove('active')
+
+        if (cnt.classList.contains(title))
+            cnt.classList.add('active')
+    })
+}
